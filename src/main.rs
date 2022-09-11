e8e78ac3220363fddbc47f99f331fe3f373408a8
Manoel Dantas <manoel.lobo@gmail.com>
2022-09-11 03:35:37 -0300

    basic scaffold, input parsing (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::env;
+
+use crate::Operation::*;
+
+const HEIGHT: isize = 60;
+
+enum Operation {
+    Forward(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Noop(u8),
+}
+
+fn parse(input: &str) -> Vec<Operation> {
+    let mut steps = Vec::<Operation>::new();
+
+    for byte in input.bytes() {
+        let step = match byte {
+            b'0' => Home,
+            b'1'..=b'9' => {
+                let distance = (byte - 0x30) as isize;
+                Forward(distance * (HEIGHT / 10))
+            }
+            b'a' | b'b' | b'c' => TurnLeft,
+            b'd' | b'e' | b'f' => TurnRight,
+            _ => Noop(byte),
+        };
+
+        steps.push(step);
+    }
+
+    steps
+}
+
+fn convert() {}
+
+fn generate_svg() {}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    let input = args.get(1).unwrap();
+    let default = format!("{}.svg", input);
+    let output = args.get(2).unwrap_or(&default);
+
+    let operations = parse(input);
+    let path_data = convert(&operations);
+    let file_content = generate_svg(&path_data);
+    svg::save(output, &file_content).unwrap();
 }
