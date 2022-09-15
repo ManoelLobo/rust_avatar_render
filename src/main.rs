33f1cd47b0f933cb3eb7573c511ea13019fdc94b
Manoel Dantas <manoel.lobo@gmail.com>
2022-09-15 16:00:42 -0300

    feat: generate svg (wip)
    - empty canvas
    - illegal byte encountered

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,104 @@
 use std::env;
 
+use svg::node::element::{
+    path::{Command, Data, Position},
+    Path, Rectangle,
+};
+use svg::Document;
+
 use crate::Operation::*;
+use crate::Orientation::*;
 
-const HEIGHT: isize = 60;
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
 
+const HOME_Y: isize = HEIGHT / 2;
+const HOME_X: isize = WIDTH / 2;
+
+const STROKE_WIDTH: usize = 5;
+
+#[derive(Debug, Clone, Copy)]
 enum Operation {
     Forward(isize),
     TurnLeft,
     TurnRight,
     Home,
     Noop(u8),
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Orientation {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Debug)]
+struct Artist {
+    x: isize,
+    y: isize,
+    direction: Orientation,
+}
+
+impl Artist {
+    fn new() -> Artist {
+        Artist {
+            direction: North,
+            x: HOME_X,
+            y: HOME_Y,
+        }
+    }
+
+    fn home(&mut self) {
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.direction {
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x += distance,
+            East => self.x -= distance,
+        }
+    }
+
+    fn turn_right(&mut self) {
+        self.direction = match self.direction {
+            North => East,
+            South => West,
+            West => North,
+            East => South,
+        }
+    }
+
+    fn turn_left(&mut self) {
+        self.direction = match self.direction {
+            North => West,
+            South => East,
+            West => South,
+            East => North,
+        }
+    }
+
+    fn wrap(&mut self) {
+        if self.x < 0 {
+            self.x = HOME_X;
+            self.direction = West;
+        } else if self.x > WIDTH {
+            self.x = HOME_X;
+            self.direction = East;
+        }
+
+        if self.y < 0 {
+            self.y = HOME_Y;
+            self.direction = North;
+        } else if self.y > HEIGHT {
+            self.y = HOME_Y;
+            self.direction = South;
+        }
+    }
 }
 
 fn parse(input: &str) -> Vec<Operation> {
@@ -33,9 +122,62 @@ fn parse(input: &str) -> Vec<Operation> {
     steps
 }
 
-fn convert() {}
+fn convert(operations: &Vec<Operation>) -> Vec<Command> {
+    let mut turtle = Artist::new();
 
-fn generate_svg() {}
+    let mut path_data = Vec::<Command>::with_capacity(operations.len());
+    let start_at_home = Command::Move(Position::Absolute, (HOME_X, HOME_Y).into());
+    path_data.push(start_at_home);
+
+    for op in operations {
+        match *op {
+            Forward(distance) => turtle.forward(distance),
+            TurnLeft => turtle.turn_left(),
+            TurnRight => turtle.turn_right(),
+            Home => turtle.home(),
+            Noop(byte) => {
+                eprintln!("warning: illegal byte encountered: {:?}", byte);
+            }
+        };
+
+        let path_segment = Command::Line(Position::Absolute, (turtle.x, turtle.y).into());
+        path_data.push(path_segment);
+
+        turtle.wrap();
+    }
+    path_data
+}
+
+fn generate_svg(path_data: Vec<Command>) -> Document {
+    let background = Rectangle::new()
+        .set("x", 0)
+        .set("y", 0)
+        .set("width", WIDTH)
+        .set("height", HEIGHT)
+        .set("fill", "#ffffff");
+
+    let border = background
+        .clone()
+        .set("fill-opacity", "0.0")
+        .set("stroke", "#cccccc")
+        .set("stroke-width", 3 * STROKE_WIDTH);
+
+    let sketch = Path::new()
+        .set("fill", "none")
+        .set("stroke", "#2f2f2f")
+        .set("stroke-width", STROKE_WIDTH)
+        .set("stroke-opacity", "0.9")
+        .set("d", Data::from(path_data));
+
+    Document::new()
+        .set("viewBox", (0, 0, HEIGHT, WIDTH))
+        .set("height", HEIGHT)
+        .set("width", WIDTH)
+        .set("style", "style=\"outline: 5px solid #800000;\"")
+        .add(background)
+        .add(sketch)
+        .add(border)
+}
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -45,6 +187,6 @@ fn main() {
 
     let operations = parse(input);
     let path_data = convert(&operations);
-    let file_content = generate_svg(&path_data);
+    let file_content = generate_svg(path_data);
     svg::save(output, &file_content).unwrap();
 }
